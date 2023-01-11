fac70fbdb182e93e9df6df7f8e58f7239a724b32
neverGiveUppppp <csy6219@naver.com>
2023-01-11 09:53:04 +0900

    login count API is added & debugging :wq

--- backend03.SpringBoot/settingweb_boot/src/main/java/com/devfun/settingweb_boot/test/settingTest.java
+++ backend03.SpringBoot/settingweb_boot/src/main/java/com/devfun/settingweb_boot/test/settingTest.java
@@ -1,25 +1,43 @@
 package com.devfun.settingweb_boot.test;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.devfun.settingweb_boot.service.StatisticService;
 
 @Controller
 public class settingTest {
-	
-	@RequestMapping("/test")
-	public ModelAndView test() throws Exception{
-		ModelAndView mav = new ModelAndView("test");
-		mav.addObject("name","devfun");
-		ArrayList<String> resultList = new ArrayList<String>();
-		resultList.add("Hello World");
-		resultList.add("설정 test");
-		resultList.add("설정 test!!!");
-		resultList.add("설정 test!!!!!");
-		mav.addObject("list",resultList);
-		return mav;
-	}
-
+    
+ 
+    @Autowired
+    private StatisticService service;
+    
+    @ResponseBody 
+    @RequestMapping("/sqlyearStatistic")
+    public Map<String, Object> sqltest(String year) throws Exception{ 
+        
+        return service.yearloginNum(year);
+    }
+    
+    @RequestMapping("/test") 
+    public ModelAndView test() throws Exception{ 
+        ModelAndView mav = new ModelAndView("test"); 
+        mav.addObject("name", "devfunpj"); 
+        List<String> resultList = new ArrayList<String>(); 
+        resultList.add("!!!HELLO WORLD!!!"); 
+        resultList.add("설정 TEST!!!"); 
+        resultList.add("설정 TEST!!!"); 
+        resultList.add("설정 TEST!!!!!"); 
+        resultList.add("설정 TEST!!!!!!"); 
+        mav.addObject("list", resultList); 
+        return mav; 
+    }
+ 
 }
